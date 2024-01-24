ae8d30e4a8bb460ec6af4668280233e82d7633a8
Darrens24 <edouard.leleux@gmail.com>
2024-01-24 12:29:15 +0100

    data is normalized for logistic regression

--- programs/logistic-regression/logreg_train.py
+++ programs/logistic-regression/logreg_train.py
@@ -5,7 +5,11 @@ from describe import Describe
 # import matplotlib.pyplot as plt
 
 
-def clean_data(data):
+def logistic_regression(data):
+    return
+
+
+def clean_normalize_data(data):
     houses = data["Hogwarts House"]
     print("houses:", houses)
     marks = data[["Astronomy", "Herbology", "Divination", "Muggle Studies",
@@ -14,12 +18,24 @@ def clean_data(data):
     print("marks:", marks)
     myDescribe = Describe(marks)
     myDescribe.print_stats()
+    print("-*-"*10)
+    print("type of myDescribe.stats:", type(myDescribe.stats))
+    print("marks.columns:", marks.columns)
+    # print("myDescribe.mean:", myDescribe.stats["Astronomy"]["mean"])
+    cleaned_data = {}
+    for subject in marks.columns:
+        mean = myDescribe.stats[subject]["mean"]
+        std = myDescribe.stats[subject]["std"]
+        cleaned_data[subject] = (marks[subject] - mean) / std
+    print("cleaned_data:", cleaned_data)
+    return cleaned_data
 
 
 def main(arg):
     data = load_dataset(arg)
     print("data:", data)
-    cleaned_data = clean_data(data)
+    normalized_data = clean_normalize_data(data)
+    result = logistic_regression(normalized_data)
     return
 
 
